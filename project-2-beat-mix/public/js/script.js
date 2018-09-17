dc274a28dda8d6532ed628dd7c4cf3fd66ce9095
John Daugherty <johndaugherty1986@gmail.com>
2018-09-17 17:11:04 -0500

    all tests for toggledrum passing

--- project-2-beat-mix/public/js/script.js
+++ project-2-beat-mix/public/js/script.js
@@ -21,3 +21,29 @@ false, false, false, false,
 false, false, false, false,
 false, false, false, false ]
 
+
+function toggleDrum(drum, num){
+
+  if (num>16 || num<0){
+    return null
+  }else{
+   switch (drum){
+  case "kicks":
+  drum = kicks;
+  break;
+  case "snares":
+  drum = snares;
+  break;
+  case "hiHats":
+  drum = hiHats;
+  break;
+  case "rideCymbals":
+  drum = rideCymbals;
+  break;
+  default:
+  console.log( "incorrect drum choice")
+  return null;
+}
+    drum[num]=!drum[num];
+  }
+}
