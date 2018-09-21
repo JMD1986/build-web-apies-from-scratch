e1ad14a0e272df56a04f875a41c088da1ed24ee4
John Daugherty <johndaugherty1986@gmail.com>
2018-09-20 23:23:21 -0500

    starting presethandler

--- project-2-beat-mix/presetHandler.js
+++ project-2-beat-mix/presetHandler.js
@@ -1,8 +1,22 @@
 // Use this presets array inside your presetHandler
 const presets = require('./presets');
+const getPreset = (index) => {
+  return presets[index] || null;
+};
+
+const createOrUpdatePreset = (index, array) => {
+  if (!presets[index]) {
+    return;
+  }
+  presets[index] = array;
+  return presets[index];
+};
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (method, index, newPresetArray) => {
+
+
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
